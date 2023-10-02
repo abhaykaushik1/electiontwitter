96febf3efd6568a814e14d80a9211c588f045cd1
ABHAY KAUSHIK <abhaykaushik@outlook.com>
2023-10-02 19:37:34 -0400

    Put attention code in appropriate functions

--- transformer.py
+++ transformer.py
@@ -1,25 +1,21 @@
 import numpy as np 
 import math
 
-# self attention
+
 L, d_k, d_v, = 0, 0, 0
-Q = np.random.randn(L, d_k)
-K = np.random.randn(L, d_k)
-V = np.random.randn(L, d_v)
-
-# need scaled values in the input in softmax function to get the equation for attention since it helps keep values in range
-scaled = np.matmul(Q, K.T) / math.sqrt(d_k)
-
-# Need mask to ensure words don't get context from words in the future
-M = np.tril(np.ones((L, L)))
-M[M == 0] = -np.infty
-M[M == 1] = 0
-
-# Now to get the matrix we input into softmax function
-s_in = scaled + M
 
 # define softmax
 def softmax(x):
     return (np.exp(x).T / np.sum(np.exp(x), axis=-1)).T
 
-attention = softmax(s_in)
+def scaled_dot_product_attention(Q, K , V, mask=None):
+    d_k = Q.shape[-1]
+    scaled = np.matmul(Q, K.T)/math.sqrt(d_k)
+
+    if mask:
+        scaled = scaled + mask
+    
+    attention = softmax(scaled)
+    new_V = np.matmul(attention, V)
+
+    return attention, new_V
