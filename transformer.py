55bf34cb4bbdfb2b66c5a88ef0061b0db7337221
ABHAY KAUSHIK <abhaykaushik@outlook.com>
2023-10-12 23:52:11 -0400

    fixed forward propogation to encodeer

--- transformer.py
+++ transformer.py
@@ -11,19 +11,23 @@ class Linear:
     weights: np.ndarray
     bias: np.ndarray
 
-    def __init__(self, in_features:int, out_features:int, bias:bool=True):
+    def __init__(self, in_features:int, out_features:int):
         self.in_features = in_features
         self.out_features = out_features
-        self.weights = np.empty([out_features, in_features])
-
-        if bias:
-            self.bias = np.empty([out_features, 1])
+        self.weights = np.random.randn(out_features,in_features) * 0.01
+        self.bias = np.random.randn(out_features,1) * 0.01
 
     def __call__(self, inp:np.ndarray) -> np.ndarray:
         return self.forward(inp)
 
-    def forward(self, inp:np.ndarray, bias:bool=True) -> np.ndarray:
-        return np.matmul(self.weights, inp.T)+self.bias if bias else np.matmul(self.weights, inp.T)
+    def forward(self, x:np.ndarray) -> np.ndarray:
+        m = []
+        for i in x:
+            Z = np.dot(i, self.weights.T) + self.bias.T
+            # Definition of ReLU
+            A = Z * (Z > 0)
+            m.append(A)
+        return np.array(m)
 
 class MultiheadAttention:
 
@@ -46,17 +50,18 @@ class MultiheadAttention:
         batch_size, sequence_length, input_dim = x.shape
         qkv = self.qkv_layer(x)
         qkv = qkv.reshape(batch_size, sequence_length, self.num_heads, 3*self.head_dim)
-        qkv = qkv.permute(0, 2, 1, 3)
-        q, k, v = qkv.chunk(3, dim=-1)
-        values, attention = scaled_dot_product(q, k, v, mask)
+        qkv = qkv.transpose((0, 2, 1, 3))
+        q, k, v = np.array_split(qkv, 3, axis=-1)
+        attention, values = scaled_dot_product_attention(q, k, v, mask)
         values = values.reshape(batch_size, sequence_length, self.num_heads*self.head_dim)
         return self.linear_layer(values)
 
 # define softmax
 def softmax(x):
-    return (np.exp(x).T / np.sum(np.exp(x), axis=-1)).T
+    exp_max = np.exp(x - np.max(x,axis=-1,keepdims=True))
+    return exp_max/np.sum(exp_max,axis=-1,keepdims=True)
 
-def scaled_dot_product_attention(Q, K, V, mask=None):
+def scaled_dot_product_attention(q, k, v, mask=None):
     """
     Q: what user is looking for
     K: what I can offer
@@ -64,22 +69,25 @@ def scaled_dot_product_attention(Q, K, V, mask=None):
 
     Each have same dimension N x 1
     """
-    d_k = Q.shape[-1]
-    scaled = np.matmul(Q, K.transpose(-1,-2)) / math.sqrt(d_k)
+    d_k = q.shape[-1]
+    dim = list(range(len(k.shape)))
+    dim[-1], dim[-2] = dim[-2], dim[-1]
+    scaled = np.matmul(q, k.transpose(dim)) / math.sqrt(d_k)
     if mask:
         scaled = scaled + mask
     attention = softmax(scaled)
-    new_V = np.matmul(attention, V)
+    new_V = np.matmul(attention, v)
     return attention, new_V
 
+if __name__ == "__main__":
+    input_dim = 1024
+    d_model = 512
+    num_heads = 8
 
-input_dim = 1024
-d_model = 512
-num_heads = 8
+    batch_size = 30
+    sequence_length = 5
+    x = np.random.randn(batch_size, sequence_length, input_dim)
 
-batch_size = 30
-sequence_length = 5
-x = np.random.randn(batch_size, sequence_length, input_dim)
-
-model = MultiheadAttention(input_dim, d_model, num_heads)
-out = model.forward(x)
+    model = MultiheadAttention(input_dim, d_model, num_heads)
+    out = model.forward(x)
+    print(out)
